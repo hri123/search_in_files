f3cf7a6574b85f3163b817938d3de68e2c6272da
hrishikesh-kumar <hrishikesh.kumar@in.ibm.com>
2017-12-06 22:44:51 +0530

    NA

--- source/hsearch.py
+++ source/hsearch.py
@@ -1,6 +1,26 @@
+# This file should only contain the logic, implementation details must be pushed into other files
 
+# Move print help to a different file
+
+# python3 hsearch.py "<search phrase>" "<files to include>"
+# e.g.:
+# python3 hsearch.py "hunt moby tartar" "/Users/H/HP/Dropbox/Kaizen/**/*.json"
+
+
+import sys
+import glob
+
+search_phrase = sys.argv[1]
+files_list = sys.argv[2]
 
 # loop through each of the files
+
+if __name__ == "__main__":
+    for filename in glob.iglob(files_list, recursive=True):
+        with open(filename, 'r') as myfile:
+
+
+            myfile.close()
 
 # split file into lines
 
